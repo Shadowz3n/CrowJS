b311601eadda5b3d00aebe35476cf406b927cd9c
Henrique Bissoli Silva <emp.shad@gmail.com>
2017-12-06 10:22:46 -0200

    Update crow.js

--- crow.js
+++ crow.js
@@ -9,30 +9,33 @@
                 [].forEach.call(crow, function(item){
                     item.classList.add(class_.split(" "));
                 });
+                return crow;
             },
             toggleClass: function(class_){
                 [].forEach.call(crow, function(item){
                     item.classList.toggle(class_.split(" "));
                 });
+                return crow;
             },
             hasClass: function(class_){
-                return [].forEach.call(crow, function(item){
-                    item.classList.contains(class_.split(" "));
+                var hasClass    = false;
+                [].forEach.call(crow, function(item){
+                    hasClass    = item.classList.contains(class_.split(" "))? true:hasClass;
                 });
+                return hasClass;
             },
             removeClass: function(class_){
                 [].forEach.call(crow, function(item){
                     item.classList.remove(class_.split(" "));
                 });
-            }
+                return crow;
+            },
         };
         return crow;
     });
-
     Crow.ready = function(callback){
         (/complete|loaded|interactive/.test(document.readyState) && document.body)? callback(Crow.ready):document.addEventListener('DOMContentLoaded', function(){callback(Crow.ready)}, false);
         return this;
     }
-
     window.Crow = Crow;
 }));
